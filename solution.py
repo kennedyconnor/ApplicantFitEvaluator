09a1a0e7fb7bbbde936b29d738a533730e08dada
kennedyconnor <kennedyconnor02@gmail.com>
2024-03-01 11:42:08 +0100

    Team Statistics Method

--- solution.py
+++ solution.py
@@ -7,8 +7,25 @@ INPUT_FILE_PATH = 'test_data.json'
 OUTPUT_FILE_PATH = 'output_data.json'
 
 def calculate_team_statistics(team_data):
-    # Calculate the mean and variange for each attribute of the team
-    pass
+    # Calculate the mean and variance for each attribute of the team
+    # Attributes are dynamically pulled from the first team member (assumed to be constant across team)
+    # return a List of two Dictionaries [means, variances]
+
+    means = {}
+    variances = {}
+    # pull list of attributes from first team member
+    attributes = team_data[0]['attributes'].keys()
+
+    for att in attributes:
+        att_values = []
+        for tm in team_data:
+            att_values.append(tm['attributes'][att])
+        means[att] = statistics.mean(att_values)
+        variances[att] = statistics.variance(att_values)
+
+    return [means, variances]
+
+
 
 def calculate_applicant_compatibility(team_stats, applicant_data):
     # Measure invididual applicant against team and compatibility
@@ -32,9 +49,7 @@ def main():
 
     print("Team Data", team_data)
     print("Applicants Data", applicants_data)
-
-
-
+    print("Team Stats", team_stats)
 
 
 if __name__ == '__main__':
