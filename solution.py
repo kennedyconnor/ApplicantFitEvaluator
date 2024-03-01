3cd3e9aaead380a006222a2ef8e7146e7cacdc4c
kennedyconnor <kennedyconnor02@gmail.com>
2024-03-01 11:13:24 +0100

    defining data / outputs, additional method ideas

--- solution.py
+++ solution.py
@@ -1,5 +1,41 @@
-# Hello, my name is Connor Kennedy. Welcome to my solution for the Datahouse Take Home Assessment.
-# I will be using the json library for python to process the JSON intake file. 
+# Hello, my name is Connor Kennedy. Welcome to my solution for the Compatability Predictor.
 
-def solution():
+import json
+import statistics
+
+INPUT_FILE_PATH = 'test_data.json'
+OUTPUT_FILE_PATH = 'output_data.json'
+
+def calculate_team_statistics(team_data):
+    # Calculate the mean and variange for each attribute of the team
     pass
+
+def calculate_applicant_compatibility(team_stats, applicant_data):
+    # Measure invididual applicant against team and compatibility
+    pass
+
+
+def main():
+
+    # Open and read JSON File
+    try:
+        with open((INPUT_FILE_PATH), 'r') as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        print(f"{INPUT_FILE_PATH} was not found.")
+        return None
+    
+    # Define key data for team and applicants
+    team_data = data['team']
+    team_stats = calculate_team_statistics(team_data)
+    applicants_data = data['applicants'] 
+
+    print("Team Data", team_data)
+    print("Applicants Data", applicants_data)
+
+
+
+
+
+if __name__ == '__main__':
+    main()
