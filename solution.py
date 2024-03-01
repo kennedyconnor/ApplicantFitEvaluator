5d2d997fa65d7dd87506abcdc0e7ea29a83668b1
kennedyconnor <kennedyconnor02@gmail.com>
2024-03-01 10:09:29 +0100

    Initial commit, file structure

---
+++ solution.py
@@ -0,0 +1,5 @@
+# Hello, my name is Connor Kennedy. Welcome to my solution for the Datahouse Take Home Assessment.
+# I will be using the json library for python to process the JSON intake file. 
+
+def solution():
+    pass
